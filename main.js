9b6bacc2858ad4865447a7f27582f104c2b96c0e
Brendan <brendanewen@gmail.com>
2020-01-08 14:50:41 +0000

    channges
    - added some javascsript to show and hide nav links
    - dpes not work atm as unexpted {
    - new js file added

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+$(".main-nav-btn").on("click", function () {
+   var element = document.getElementById(nav_li);
+   if (element.hasClass("show") {
+    $this.removeClass("show");
+   } else {
+    $this.addClass("show");
+   }
+});
