fd6194421822f9488374efeb37a6da23a78b4a1b
Brendan <brendanewen@gmail.com>
2020-01-28 10:18:45 +0000

    js changes
    trying to find a way to show and hide elements. However function /getElementByClassName is not a function apparently.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-
-
-$(".main-nav-btn").on("click", function () {
-   var element = document.getElementById(nav_li);
-   if (element.hasClass("show") {
-    $this.removeClass("show");
-   } else {
-    $this.addClass("show");
-   }
-});
+function changeClass(){
+  //grab current class of th eli element
+  var showHideElement = document.getElementByClassName("nav_li");
+  if(showHideElement.classList.contains("show")){
+    document.getElementByClassName("nav_li").classList.remove("show");
+    document.getElementByClassName("nav_li").classList.add("hide");
+  }else{
+    document.getElementByClassName("nav_li").classList.add("show");
+    document.getElementByClassName("nav_li").classList.remove("hide");
+  };
+  console.log("test");
+};
