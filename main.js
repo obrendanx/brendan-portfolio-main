51aab5720cee28f246ab885c5ee261176e6707b4
Brendan <brendanewen@gmail.com>
2020-03-25 10:59:57 +0000

    navbar changes
    - add header info
    - need to fix text showing when bar is active using same js as navbar

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ function changeClass(){
   if(showHideElement.classList.contains("show")){
     document.getElementById("main-nav-ul").classList.remove("show");
     document.getElementById("main-nav-ul").classList.add("hide");
+    document.getElementById("header-info").classList.remove("hide");
+    document.getElementById("header-info").classList.add("show");
   }else{
     document.getElementById("main-nav-ul").classList.add("show");
     document.getElementById("main-nav-ul").classList.remove("hide");
+    document.getElementById("header-info").classList.remove("show");
+    document.getElementById("header-info").classList.add("hide");
   };
   console.log("test");
 };
